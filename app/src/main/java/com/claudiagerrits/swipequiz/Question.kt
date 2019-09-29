37b66efb7163e7a985f824112599ef2a16c8df33
MSI\Claudia <rtclaudiag@gmail.com>
2019-09-29 17:28:00 +0200

    Initial commit

---
+++ app/src/main/java/com/claudiagerrits/swipequiz/Question.kt
@@ -0,0 +1,6 @@
+package com.claudiagerrits.swipequiz
+
+data class Question(
+    val question: String,
+    val isCorrect: Boolean
+)
---
+++ app/src/main/java/com/claudiagerrits/swipequiz/QuestionAdapter.kt
@@ -0,0 +1,11 @@
+package com.claudiagerrits.swipequiz
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import androidx.recyclerview.widget.RecyclerView
+
+class QuestionAdapter {
+}
